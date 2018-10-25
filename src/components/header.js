2fe1aeeb81609375e606a78f2bf5908a2b82dc62
Thanaphon Chaysawat <23chaysawat@Thanaphons-MacBook-Pro.local>
2018-10-25 14:47:55 -0700

    Test Animations, Prop&State

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-      <div className="header-group">
-        <p className="logo">∞</p>
-        <h1 className="app-name">aka <b>Tatuu</b></h1>
-        <Link to="/works">Works</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
-  </div>
-)
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true });
+    } else {
+      this.setState({hasScrolled: false });
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled? 'header scrolled':'header'}>
+        <div className="header-group">
+          <p className="logo">∞</p>
+          <h1 className="app-name">aka <b>Tatuu</b></h1>
+          <Link to="/works">Works</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+    </div>
+    );
+  }
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import Header from '../components/header'
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
@@ -15,14 +14,34 @@ const IndexPage = () => (
         <p className="breif-me">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <Link to="/about/">More about me  
-          <button className="btn arrow">
-            GO
-          </button>
+        <Link to="/about/" className="link-to-works">
+          <div>More about me </div> 
+          <div className="btn-group">
+            <span className="box"></span>
+            <span className="text">Let's Go!</span>
+          </div>
         </Link>
+        <Btn />
       </div>
     </div>
   </Layout>
 )
+class Btn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: false};
+  }
+  render() {
+    return (
+      <div 
+        className={this.state.active? 'hidden-box show':'hidden-box'}
+        onMouseEnter={() => this.setState({active : true})}
+        onMouseLeave={() => this.setState({active : false})}
+      >
+        <h4 className="hidden-text">Hello! I'm Tatuu!</h4>
+      </div>
+    );
+  }
+}
 
 export default IndexPage
